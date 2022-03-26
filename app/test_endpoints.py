51e83b6c21b3d83cc8e5cbfbc99262449356f0f3
Matthew Stiller <30756911+myskillz2000@users.noreply.github.com>
2022-03-26 16:07:33 -0400

    Modified due to pre-commit.

--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -6,11 +6,11 @@ client = TestClient(app)
 def test_get_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert "text/html" in response.headers['content-type'] 
+    assert "text/html" in response.headers['content-type']
 
 
 def test_post_home():
     response = client.post("/")
     assert response.status_code == 200
-    assert "application/json" in response.headers['content-type'] 
+    assert "application/json" in response.headers['content-type']
     assert response.json() =={"hello": "world"}
